9b7cbcc9bdc117e5551968483d4f68b614f16f77
adriellysilva50 <adriellyalbuquerque755@gmail.com>
2024-08-20 12:36:21 -0300

    hj 20/08

--- api.py
+++ api.py
@@ -1,6 +1,9 @@
 from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask (__name__)
+
+
 
 livros = [
     {'id':1, 'titulo': '1984' , 'autor' : 'George orwell'},
